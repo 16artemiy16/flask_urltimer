037163bb3749d67ccb14a8a8ce54321f48eaa430
Artem Polikarpov <artemiypolikarpov@gmail.com>
2022-05-15 16:50:33 +0300

    Integrate ChartJS to VueJS front

--- flask_urltimer/ui/src/router/index.ts
+++ flask_urltimer/ui/src/router/index.ts
@@ -1,6 +1,12 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Stats from '@/views/stats-page.vue';
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: Stats,
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(process.env.BASE_URL),
---
+++ flask_urltimer/ui/src/views/stats-page.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Legend, Tooltip, ArcElement } from 'chart.js'
+
+ChartJS.register(Title, ArcElement, Legend, Tooltip)
+
+const chartData = {
+  labels: [
+    'Start - Mark 1',
+    'Mark 1 - Mark 2',
+    'Mark 2 - End'
+  ],
+  datasets: [{
+    label: 'Total: 6.2s',
+    data: [2, 1, 3.2],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+  }]
+};
+
+</script>
+
+
+<template>
+  <div class="chart-wrapper">
+    <Pie :chart-data="chartData"  />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.chart-wrapper {
+  width: 500px;
+  height: 500px;
+}
+</style>
